042c82107d7f60e5bb7cbd1d0c22b3229dce3a8f
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2025-03-05 22:37:58 +0300

    Project Update

---
+++ src/Clients/Youtube.Client.AspNetCore/Extensions/DependencyInjectionExtebsions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using YoutubeApi.Client.Clients;
+using YoutubeApi.Client.Interfaces;
+using YoutubeApi.Client.Models;
+
+namespace Youtube.Client.AspNetCore.Extensions
+{
+    public static class DependencyInjectionExtebsions
+    {
+        public static IServiceCollection AddYoutubeClient(this IServiceCollection services,YoutubeClientOptions options)
+        {
+            YoutubeClientOptions youtubeClientOptions = new YoutubeClientOptions();
+            services.AddScoped<IYoutubeClient, YoutubeClient>();
+            services.AddHttpClient<IYoutubeClient, YoutubeClient>((sp, client)=>
+                {
+                client.BaseAddress = new Uri(options.HostUri);
+            });
+            return services;
+        }
+    }
+}
---
+++ src/Clients/YoutubeApi.Client/Models/YoutubeClientOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeApi.Client.Models
+{
+   public class YoutubeClientOptions
+    {
+        public string HostUri { get; set; }
+    }
+}
